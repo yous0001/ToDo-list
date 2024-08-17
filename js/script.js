f80b99b60c5cfd3738a65201f0bc696f229ff38e
Yousef emad mikhail <mad33833@gmail.com>
2024-08-17 08:15:53 +0300

    add task

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const inputBox=document.getElementById("input-box")
+const listContainer=document.getElementById("list-container")
+const addButton=document.getElementById("add-button")
+
+addButton.addEventListener('click',addTask)
+
+listContainer.innerHTML=localStorage.getItem("tasks")
+
+function addTask(){
+    if(inputBox.value==""){
+        document.getElementsByClassName("alert")[0].style.display="block"
+
+    }
+    else{
+        document.getElementsByClassName("alert")[0].style.display="none"
+        let li =document.createElement("li")
+        li.innerHTML=inputBox.value
+        listContainer.appendChild(li)
+        let span =document.createElement("span")
+        span.innerHTML="\u00d7"
+        li.appendChild(span)
+        inputBox.value=""
+        
+    }
+    saveData()
+}
+
+
+function saveData(){
+    localStorage.setItem("tasks",listContainer.innerHTML)
+}
