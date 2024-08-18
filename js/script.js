8f79b944a49595d72f6a10a01521bed48e65e00a
Yousef emad mikhail <mad33833@gmail.com>
2024-08-18 15:38:38 +0300

    feat:enter task by press enter

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ const inputBox=document.getElementById("input-box")
 const listContainer=document.getElementById("list-container")
 const addButton=document.getElementById("add-button")
 
+
 addButton.addEventListener('click',addTask)
+
+document.addEventListener("keypress",function(e){
+    if (e.key === "Enter"){
+        addTask()
+    }
+})
 
 listContainer.innerHTML=localStorage.getItem("tasks")
 
@@ -13,14 +20,21 @@ function addTask(){
     }
     else{
         document.getElementsByClassName("alert")[0].style.display="none"
+
         let li =document.createElement("li")
         li.innerHTML=inputBox.value
         listContainer.appendChild(li)
+
+        let update =document.createElement("i")
+        update.classList.add("fa-solid","fa-pen")
+        li.appendChild(update)
+
         let span =document.createElement("span")
         span.innerHTML="\u00d7"
         li.appendChild(span)
+
         inputBox.value=""
-        
+
     }
     saveData()
 }
@@ -33,6 +47,11 @@ listContainer.addEventListener('click',function(e){
     else if(e.target.tagName==="SPAN"){
         e.target.parentElement.remove()
         saveData()
+    }
+    else if(e.target.tagName==="I"){
+        inputBox.value=e.target.parentElement.innerText.split("\u00d7")[0]
+        // e.target.parentElement.remove()
+        // saveData()
     }
 },false)
 
