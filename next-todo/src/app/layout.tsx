44bf8ec1eecb7937754ec9fd327b7277a19ff23c
Yousef emad mikhail <mad33833@gmail.com>
2025-11-21 11:10:08 +0200

    fix console issue

--- next-todo/src/app/layout.tsx
+++ next-todo/src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-[#020617] text-white antialiased`}
       >
         <ThemeProvider>
