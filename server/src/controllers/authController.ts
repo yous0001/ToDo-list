2823f33709a693aea2ebdb5179dc83edbd6c19bd
Yousef emad mikhail <mad33833@gmail.com>
2025-11-26 09:12:18 +0200

    fix auth issue

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -177,10 +177,8 @@ export const getProfile = async (req: Request, res: Response) => {
   try {
     const users = await getUsersCollection();
     const objectId = parseObjectId(userId);
-    if (!objectId) {
-      return res.status(400).json({ error: "Invalid user id" });
-    }
-    const user = await users.findOne({ _id: objectId });
+    const query = objectId ? { _id: objectId } : { id: userId };
+    const user = await users.findOne(query);
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
